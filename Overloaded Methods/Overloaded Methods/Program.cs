59028ef8bb7bf3f05170c38d56d7a4eff4f92dd7
Engr. Atta Ullah Khan <attaullahkhan43878@gmail.com>
2023-06-08 20:26:25 +0500

    Overloaded Methods

---
+++ Overloaded Methods/Overloaded Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Overloaded_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.show();
+            student.show("My age is: ");
+            student.show(22, "Your age is: ");
+        }
+    }
+}
---
+++ Overloaded Methods/Overloaded Methods/Student.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Overloaded_Methods
+{
+    class Student
+    {
+        private int age;
+
+        public Student(int a) 
+        { 
+            age = a;
+        }
+
+        public Student() 
+        {
+            age = 20;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(age);
+        }
+
+        public void show (string message)
+        {
+            Console.WriteLine(message + age);
+        }
+
+        public void show(int a, string message)
+        {
+            age = a;
+            Console.WriteLine(message + age);
+        }
+    }
+}
