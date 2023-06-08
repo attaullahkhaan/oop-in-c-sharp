46c984611107767cdad974b74daedbaf2d1e2bc2
Engr. Atta Ullah Khan <attaullahkhan43878@gmail.com>
2023-06-08 20:31:13 +0500

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.SetAgeData(20);
+            std.SetMarks(60);
+            std.SetName("Hello welcome to C# Programming ");
+
+            std.detail();
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Student.cs
@@ -0,0 +1,46 @@
+﻿namespace Encapsulation
+{
+    class Student
+    {
+        private int age;
+        private int marks;
+        private string name;
+
+        public void SetAgeData (int a)
+        {
+            if (a > 0)
+            {
+                age = a;
+            }
+            else
+            {
+                Console.WriteLine("you're entered an invalid value, please try again");
+            }
+        }
+
+        public void SetMarks (int m)
+        {
+            if (m > 0)
+            {
+                marks = m;
+            }
+        }
+
+        public void SetName (string n)
+        {
+            if (!string.IsNullOrEmpty(n))
+            {
+                name = n;
+            }
+            else
+            {
+                Console.WriteLine("You cannot empty or null value in name");
+            }
+        }
+
+        public void detail()
+        {
+            Console.WriteLine("Name = {0} Age = {1} Marks = {2}", name, age, marks);
+        }
+    }
+}
