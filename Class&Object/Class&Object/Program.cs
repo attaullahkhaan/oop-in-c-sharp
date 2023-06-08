cbcee098be14e9680ab8a975251753433081d807
Engr. Atta Ullah Khan <attaullahkhan43878@gmail.com>
2023-06-08 21:01:41 +0500

    Classes and Objects

---
+++ Class&Object/Class&Object/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Class_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating Car Object
+            Car car = new Car();
+            Console.WriteLine(car.color);
+
+            // Creating Car1 Object 
+            Car1 car1 = new Car1();
+            Console.WriteLine(car1.color + " " + car1.maxSpeed);
+        }
+    }
+
+    class Car
+    {
+        public string color = "White";
+    }
+}
