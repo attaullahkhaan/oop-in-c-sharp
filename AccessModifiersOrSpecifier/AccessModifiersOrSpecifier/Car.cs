1dbe3f41f599cf349461524eaf5c69b222fcf52f
Engr. Atta Ullah Khan <attaullahkhan43878@gmail.com>
2023-06-08 20:43:23 +0500

    Access Modifiers Or  Specifier

---
+++ AccessModifiersOrSpecifier/AccessModifiersOrSpecifier/Car.cs
@@ -0,0 +1,33 @@
+﻿public class Car
+{
+    private string privateField; // Private field
+    public string publicField; // Public field
+    protected string protectedField; // Protected field
+    internal string internalField; // Internal field
+    protected internal string protectedInternalField; // Protected Internal field
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Private Method");
+    }
+
+    public void PublicMethod()
+    {
+        Console.WriteLine("Public Method");
+    }
+
+    protected void ProtectedMethod()
+    {
+        Console.WriteLine("Protected Method");
+    }
+
+    internal void InternalMethod()
+    {
+        Console.WriteLine("Internal Method");
+    }
+
+    protected internal void ProtectedInternalMethod()
+    {
+        Console.WriteLine("Protected Internal Method");
+    }
+}
---
+++ AccessModifiersOrSpecifier/AccessModifiersOrSpecifier/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+
+        // Accessing members using different access modifiers
+        myCar.publicField = "Public Field"; // Accessible
+        myCar.PublicMethod(); // Accessible
+
+        // Uncomment the lines below to see access restrictions
+        //myCar.privateField = "Private Field"; // Not accessible
+        //myCar.PrivateMethod(); // Not accessible
+
+        //myCar.protectedField = "Protected Field"; // Not accessible
+        //myCar.ProtectedMethod(); // Not accessible
+
+        //myCar.internalField = "Internal Field"; // Accessible
+        //myCar.InternalMethod(); // Accessible
+
+        //myCar.protectedInternalField = "Protected Internal Field"; // Accessible
+        //myCar.ProtectedInternalMethod(); // Accessible
+
+        Console.ReadLine();
+    }
+}
